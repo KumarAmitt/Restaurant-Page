edd6c052e0ab95aeb2bf9ea8b82f82590be4a0e2
KumarAmitt <amitt2015@outlook.com>
2021-04-20 23:25:14 +0530

    Set the background image

--- src/index.js
+++ src/index.js
@@ -1,8 +1,2 @@
 import './stylesheets/style.scss'
-import chow from './images/chow.jpeg'
 
-console.log('Hello');
-console.log('From Dev server')
-
-document.body.style.backgroundImage = `url(${chow})`;
-document.body.style.backgroundSize = 'cover'
