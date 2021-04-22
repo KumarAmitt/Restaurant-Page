a936743063eaf3c9dc1b6d6ed3ff18e9f999f8c8
KumarAmitt <amitt2015@outlook.com>
2021-04-22 18:41:12 +0530

    Add menu list

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,9 +1,36 @@
 import {createElement} from "./tools";
 
-const menuDesc = '24 ABC Street, XYZ Road, JAMSHEDPUR'
+const menuItems = {
+  starter: {
+    'Hot and Sour soup': 99,
+    'Sweet corn soup': 89,
+    'Crispy babycorn chilly': 159,
+    'Garlic Paneer': 159
+  },
+  tandoor: {
+    'Paneer Angaara tikka': 189,
+    'Tandoori chicken': 169,
+    'Achari fish tikka': 209
+  },
+  rice: {
+    'chicken fried rice': 159,
+    'egg fried rice': 129,
+    'veg fried rice': 99
+  }
+}
 
 export const menu = ({appendTo}) => {
   const menuPage = createElement({appendTo: appendTo, tag: 'div'})
   createElement({appendTo: menuPage, tag: 'h1', cls: 'title', txt: 'Menu'})
-  createElement({appendTo: menuPage, tag: 'p', cls: 'desc', txt: menuDesc})
+
+  for(let ctg in menuItems){
+    const desc = createElement({appendTo: menuPage, tag: 'p', cls: 'desc'})
+    createElement({appendTo: desc, tag: 'h3', cls: 'category', txt: ctg});
+    let ul = createElement({appendTo: desc, tag: 'ul'})
+
+    for(let item in menuItems[ctg]){
+      let li = createElement({appendTo: ul, tag: 'li', txt: item})
+      createElement({appendTo: li, tag: 'span', txt: menuItems[ctg][item]})
+    }
+  }
 }
