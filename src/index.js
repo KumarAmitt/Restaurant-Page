398698571448cd15235a1c263da43cfdb2e4a5ec
KumarAmitt <amitt2015@outlook.com>
2021-04-23 16:02:53 +0530

    Fix eslint Offenses

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './stylesheets/style.scss';
-import kickApp from "./js/kick";
+import kickApp from './js/kick';
 
 const content = document.getElementById('content');
-kickApp({parent: content});
-
+kickApp({ parent: content });
--- src/js/kick.js
+++ src/js/kick.js
@@ -1,16 +1,15 @@
-import nav from "./nav";
-import cover from "./cover";
-import feature from "./feature";
-import main from "./main";
-import footer from "./footer";
+import nav from './nav';
+import cover from './cover';
+import feature from './feature';
+import main from './main';
+import footer from './footer';
 
-
-const kickApp = ({parent}) => {
+const kickApp = ({ parent }) => {
   nav({ appendTo: parent });
   cover({ appendTo: parent });
   feature({ appendTo: parent });
   main({ appendTo: parent });
   footer({ appendTo: parent });
-}
+};
 
 export default kickApp;
